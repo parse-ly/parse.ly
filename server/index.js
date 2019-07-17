52c3a75f74e3c8f1f71244949a73034e87343908
clydebates <a.bates1993@gmail.com>
2019-07-17 13:43:11 -0500

    Install dotenv

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -7,6 +8,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../client/dist')));
 // Parser middleware
 app.use(bodyParser.json());
+
+// GET sent from search function
+app.get('/search/:artist', (req, res) => {
+
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
