41127ffec084966a61ccc1840e366dad4b0705c6
clydebates <a.bates1993@gmail.com>
2019-07-17 12:22:23 -0500

    Small typo fixes, add "type=button" to each button

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types;'
+import PropTypes from 'prop-types';
 
 const Search = (props) => {
   return (
     <div className="search-bar">
       <input className="form-control" type="text" />
-      <button className="btn search">
+      <button type="button" className="btn search">
         {/* OnClick/onSearch function needed */}
       </button>
-      <button className="btn happy">
+      <button type="button" className="btn happy">
         {/* Happy Emoji button */}
       </button>
-      <button className ="btn sad">
+      <button type="button" className="btn sad">
         {/* Sad or Crying Emoji button */}
       </button>
     </div>
-  )
+  );
 }
 
 Search.propTypes = {
