cf500dbd9d7e18779bbe4909ea6d56f87e752efc
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-07-18 14:55:33 -0500

    Adds path routing on server side for "/*"

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 // Parser middleware
 app.use(bodyParser.json());
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
