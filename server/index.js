a9a0f0642433d350edd34f8d0ee579b690bb5553
clydebates <a.bates1993@gmail.com>
2019-07-19 13:41:08 -0500

    Add process.env.PORT to console log

--- server/index.js
+++ server/index.js
@@ -377,5 +377,5 @@ app.get('/search/:artist', (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on port 3000');
+  console.log(`listening on port ${process.env.PORT || 3000}`);
 });
