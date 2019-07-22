3a4f5f4ad5cda217f355216940182f6e9b9201ea
clydebates <a.bates1993@gmail.com>
2019-07-21 21:11:51 -0500

    Send get requests for top ten songs of both positive and negative

--- client/src/topten.jsx
+++ client/src/topten.jsx
@@ -1,16 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 import Navigation from './navbar.jsx';
 
-const TopTen = (props) => {
-  return (
-    <React.Fragment>
-      <Navigation />
-    <ul class="list-group">
-      {/* Map Song List Entry ??  */}
-      {/* Include Song Video Player */}
-    </ul>
-  </React.Fragment>
-  )
-};
+class TopTen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      positive: [],
+      negative: [],
+    };
+    this.getSongs = this.getSongs.bind(this);
+  }
+
+  componentDidMount() {
+    this.getSongs();
+  }
+
+  getSongs() {
+    axios.get('/topten/positive')
+      .then((response) => {
+        this.setState({
+          positive: response.data,
+        });
+      })
+      .catch(err => console.error(err));
+    axios.get('/topten/negative')
+      .then((response) => {
+        this.setState({
+          negative: response.data,
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navigation />
+        <ul className="list-group">
+        {/* Map Song List Entry ??  */}
+        {/* Include Song Video Player */}
+      </ul>
+      </React.Fragment>
+    );
+  }
+}
 
 export default TopTen;
